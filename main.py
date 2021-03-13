44d36fc18e43d27c2371371abbc8569e8abab698
Replit user <replituser@example.com>
2021-03-13 08:20:42 +0000

    Created a simple user defined function and call it 3 times.

---
+++ main.py
@@ -0,0 +1,11 @@
+#Simple program to coment on the inputed sound
+#First create the function using def instruction
+def listen_for_sound():
+  print("What sound did I hear?")
+  sound=input()
+  
+  print("That was a loud",sound)
+listen_for_sound()
+listen_for_sound()
+listen_for_sound()
+print("\n\t\t\tEnd")
